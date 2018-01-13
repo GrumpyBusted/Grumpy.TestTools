458921e0a5ea27c3ec53f52f56a3ccec7a0d8586
Anders Busted-Janum <abja@sdc.dk>
2018-01-13 12:29:26 +0100

    Added project etc.

---
+++ Grumpy.TestTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Grumpy.TestTools")]
+[assembly: AssemblyDescription("Collection of test tools and references to other packages and libraries that I use for my test cases.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Grumpy.TestTools")]
+[assembly: AssemblyCopyright("Copyright © Anders Busted-Janum 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("8932a208-6bd7-48ff-baff-8d9b3660b6f2")]
+[assembly: AssemblyVersion("1.0.*")]
